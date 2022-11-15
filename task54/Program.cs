7204544068912c165fcddad259bf0dc1712b65bb
Alena <aln.evskv.1@mail.ru>
2022-11-15 18:57:39 +0500

    add task

---
+++ task54/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу,
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void Main()
+{
+    Console.WriteLine("Введите количество строк");
+    int rows = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов");
+    int columns = int.Parse(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+    GetArray(array);
+    PrintArray(array);
+    Console.WriteLine();
+    SortArray(array);
+    PrintArray(array);
+
+}
+
+void GetArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void SortArray(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+        {
+            for (int z = 0; z < numbers.GetLength(1) - 1; z++)
+            {
+                if (numbers[i, z] < numbers[i, z + 1])
+                {
+                    int temp = 0;
+                    temp = numbers[i, z];
+                    numbers[i, z] = numbers[i, z + 1];
+                    numbers[i, z + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Main();
---
+++ task56/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу,
+// которая будет находить строку с наименьшей суммой элементов.
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов.
+
+void Main()
+{
+    Console.WriteLine("Введите количество строк");
+    int rows = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов");
+    int columns = int.Parse(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+
+    if (rows == columns)
+    {
+        GetArray(array);
+        PrintArray(array);
+        Console.WriteLine();
+        SumNumbers(array);
+    }
+    else
+    {
+        Console.Write("Ошибка! Количество столбцов должно быть равно количесству строк.");
+    }
+
+
+}
+
+void GetArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void SumNumbers(int[,] numbers)
+{
+    int minsum = Int32.MaxValue;
+    int indexLine = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            sum = sum + numbers[i, j];
+        }
+        if (sum < minsum)
+        {
+            minsum = sum;
+            indexLine++;
+        }
+    }
+    Console.WriteLine($"Строка с наименьшей суммой элементов -> {indexLine}");
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Main();
